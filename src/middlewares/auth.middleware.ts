da777b76708a6c6ef29324ae9ff247a2ce02e02e
sauce-git <sauce@mandacode.com>
2024-07-03 13:59:52 +0900

    Add logger configuration and middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,23 +1,33 @@
-import { MiddlewareNext, Request, Response } from "hyper-express";
+import { MiddlewareNext, Request, Response } from 'hyper-express';
 import cookie from 'cookie';
+import signature from 'cookie-signature';
+import { config, redis } from '..';
 
-export function authenticate(req: Request, res: Response, next: MiddlewareNext) {
+export async function authenticate(
+  req: Request,
+  res: Response,
+  next: MiddlewareNext
+) {
   const reqCookie = req.headers.cookie;
   if (!reqCookie) {
     res.status(401).send('Unauthorized');
     return;
   }
-  const parsedCookie = cookie.parse(reqCookie); 
-  const session = parsedCookie['SID'];
-  console.log('reqCookie', reqCookie);
-  
-  console.log(parsedCookie, session);
-  if (!session) {
+  const decodedCookie = cookie.parse(reqCookie, {
+    decode: (value) => signature.unsign(value, config.session.secret) || value,
+  });
+
+  const sid = decodedCookie[config.session.name];
+  if (!sid) {
     res.status(401).send('Unauthorized');
     return;
   }
-  req.session = {
-    uuid: session
-  };
+  const uuid  = await redis.get(sid);
+  if (!uuid) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+
+  req.params.uuid = uuid;
   next();
 }
